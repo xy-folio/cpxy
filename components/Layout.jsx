26b966725d6e6abd94f0319c32fb390288f71c33
Chloe.P <C.poonxy15@gmail.com>
2021-11-21 21:54:25 +0800

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header } from './';
+
+
+const Layout = ({children}) => {
+    return (
+        <div>
+            <Header />
+            {children}
+        </div>
+    )
+}
+
+export default Layout
