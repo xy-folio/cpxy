32b488c81f842390fd5d48c807a3a9b3d154f852
Chloe.P <C.poonxy15@gmail.com>
2021-11-23 02:13:35 +0800

    Enter

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
 import '../styles/globals.scss';
 import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
+  
   return (
     <Layout>
       <Component {...pageProps} />
