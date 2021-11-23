b46d1dddaad16d7442ebc74bb247ea61c7fbb90b
Chloe.P <C.poonxy15@gmail.com>
2021-11-23 14:27:13 +0800

    Updated next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
+  reactStrictMode: true,
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
+    buildExcludes: [/middleware-mamifest.json$/]
+    //disable: process.env.NODE_ENV === "development",
   },
 });
